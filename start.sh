d6dd6371fae1f2ecdf457c7a8c505e0a77ae772f
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-03-07 12:42:59 +0000

    remove rel

--- start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-cd `dirname $0`
-exec erl -pa $PWD/ebin -pa $PWD/deps/*/ebin -pa $PWD/deps/*/include \
-    -sname nitrogen \
-    -setcookie ec_master \
-    -eval 'application:start(ec_web)'
