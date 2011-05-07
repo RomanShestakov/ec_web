80666d5ce098dd99e187e3ea2f3c052581551d41
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-05-07 10:33:10 +0100

    fx

--- apps/view/src/index_fns.erl
+++ apps/view/src/index_fns.erl
@@ -10,18 +10,20 @@ get_schedule_rundates() ->
 select(RunDate, RegExpr) ->
     io:format("Run Select ~p ~p~n", [RunDate, RegExpr]),
     G = ec_cli:get_graph(list_to_atom(RunDate)),
-
+    %% get all vertixes
     Vs = mdigraph:vertices(G),
 
     %%?PRINT(Vs),
 
-    CC = [mdigraph:vertex(G, V) || V <- Vs],
+    VertexInfo = [mdigraph:vertex(G, V) || V <- Vs],
 
     %%?PRINT(CC),
 
     %%S = ec_db:select(list_to_atom(RunDate), RegExpr),
     P = ec_counter:start_counter(1),
-    [[{data, ec_counter:next(P)}, N, RunDate, atom_to_list(L#job.state)] || {N, L} <- CC , L =/= []].
+    [[{data, ec_counter:next(P)}, N, RunDate, atom_to_list(L#job.state),
+      L#job.start_time, L#job.end_time,
+      L#job.depends_on] || {N, L} <- VertexInfo, L =/= []].
     
 clean(RunDate, Name) ->
     io:format("Run Clean ~p ~p~n", [RunDate, Name]),
