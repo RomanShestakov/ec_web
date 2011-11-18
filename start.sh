24f7a3f94a705bd3c931e3228d36f16e5632ebb7
Roman Shestakov <romanshestakov@yahoo.co.uk>
2011-11-18 00:09:00 +0000

    change cookie to rs

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 cd `dirname $0`
 exec erl -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin -name ec_cli@127.0.0.1 \
-    -setcookie ec_master \
+    -setcookie rs \
     -eval 'application:start(view)'
