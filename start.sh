5cf725d3780e0231299e46a3a085afa192085d9f
Roman Shestakov <romanshestakov@yahoo.co.uk>
2012-02-13 20:39:04 +0000

    add master host

--- start.sh
+++ start.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 cd `dirname $0`
-exec erl -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin -name ec_cli@127.0.0.1 \
+exec erl -pa $PWD/apps/*/ebin -pa $PWD/deps/*/ebin -name ec_cli@rs.home \
     -setcookie rs \
     -eval 'application:start(view)'
+    -config ebin/view
